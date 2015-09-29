63846c30cccbbf6221de47dd631adf24d856e620
skwan <skwan@microsoft.com>
2015-09-28 17:06:46 -0700

    Force https

--- WebApp-GroupClaims-DotNet/Global.asax.cs
+++ WebApp-GroupClaims-DotNet/Global.asax.cs
@@ -17,5 +17,12 @@ namespace WebAppGroupClaimsDotNet
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {       
+            if (HttpContext.Current.Request.IsSecureConnection.Equals(false))
+            {
+                Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"] + HttpContext.Current.Request.RawUrl);
+            }
+        }
     }
 }
